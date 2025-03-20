a3dff4f15684223532f392e3ab5053aa5b69d5f0
mariocq23 <72974070+mariocq23@users.noreply.github.com>
2025-03-20 00:48:05 -0600

    Add files via upload

---
+++ context/ordenes/OrdenDetalle.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace context.ordenes
+{
+    public class OrdenDetalle
+    {
+        public int Id { get; set; }
+        public int IdOrden { get; set; }
+        public Orden Orden { get; set; }
+
+        [Required]
+        public string Producto { get; set; }
+
+        public int Cantidad { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal PrecioUnitario { get; set; }
+    }
+}
